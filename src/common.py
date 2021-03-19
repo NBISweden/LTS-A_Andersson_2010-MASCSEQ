997d1644fe0f15ebefbcb5dce7628be5adf1119a
johnne <john.sundh@scilifelab.se>
2021-03-19 07:40:22 +0100

    Add dammit input function

--- src/common.py
+++ src/common.py
@@ -6,3 +6,11 @@ import pandas as pd
 def read_samples(f):
     df = pd.read_csv(f, index_col=0)
     return df.to_dict(orient="index")
+
+
+def dammit_input(wildcards):
+    if wildcards.assembler == "transabyss":
+        i = "merged.fa"
+    elif wildcards.assembler == "trinity":
+        i = "Trinity.fasta"
+    return f"results/{wildcards.assembler}/{wildcards.sample}/{i}"
