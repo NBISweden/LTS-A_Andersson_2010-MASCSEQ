106cdbe6c007b31236c97e0b52e001a2d4296779
bengts <bengt.sennblad@scilifelab.se>
2021-03-26 16:52:19 +0100

    Re-added prependX functions to src/common.py

--- src/common.py
+++ src/common.py
@@ -14,3 +14,21 @@ def dammit_input(wildcards):
     elif wildcards.assembler == "trinity":
         i = "Trinity.fasta"
     return f"results/{wildcards.assembler}/{wildcards.sample}/{i}"
+
+""" It is good keep the code and the results separate, so
+    we run the workflow in a separate working directory. However,
+    we sometimes need to access files in the code folder. This
+    function prepends a file path with the current path to the workflow
+    base directory (kept in snakemake variable workflow.basedir).
+    Functions from the python module os are used to ascertain as
+    a correct path."""
+def prependWfd(path):
+    return(os.path.normpath(os.path.join(workflow.basedir, path)))
+
+""" Mostly, snakemake uses relative paths (to the working directory)
+    when referring to, e.g., input and output files. However, when
+    soft-linking (creating aliases) from external file, full paths is
+    required by the system. This function prepends the path to the current
+    working directory (accessed by the python function os.getcwd()). """
+def prependPwd(path):
+    return(os.path.normpath(os.path.join(os.getcwd(), path)))
