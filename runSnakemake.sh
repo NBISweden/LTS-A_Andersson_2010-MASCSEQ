6b19ee761e6f1b9f34db44b8527da75997c01a1b
bengts <bengt.sennblad@scilifelab.se>
2021-03-26 16:44:01 +0100

    Change name to runSnakemake.sh. Add --usesingularity. Remove move of cluster-file

--- runSnakemake.sh
+++ runSnakemake.sh
@@ -5,7 +5,7 @@ set -e
 # Usage: bash path/to/doSnakemake.sh [additional snakemake options] [optional snakemake output file]
 
 # get the workflow base directory (=where this script reside)
-DIR=${0%doSnakemake.sh}
+DIR=${0%runSnakemake.sh}
 
 # Default conda is currently a bit unstable, so we want to use the alternative
 # mamba implementation instead
@@ -21,36 +21,22 @@ fi
 # Set up a conda env for snakemake + mamba -- so snakemake also uses mamba
 snakemake="nbis_project_snakemake"
 if [[ "$(mamba info -e | awk -v var=$snakemake '{if($1==var)print "found"}')" != "found" ]]; then
-    mamba env create --file $DIR/envs/snakemake.yaml
+    mamba env create --file $DIR/environent.yaml
 fi
 snakemake=$(mamba info -e | awk -v var=$snakemake '{if($1==var)print $2}')
 eval "$(conda shell.bash hook)"
 conda activate $snakemake
 
 # # Allow for either running on cluster or on local computer/laptop
-# if [ "$CLUSTER" = "rackham" ]; then # Change/add cluster name if needed
-#     echo "Running on rackham"
-#     # To run on cluster, a cluster config file is needed. If not present, copy
-#     # from workflow base directory. Update the contact email at the same time.
-#     if [[ ! -f cluster.yaml ]]; then
-# 	echo "No cluster.yaml found in present directory"
-# 	echo "Do you want me to set this directory up as a Slamdunk analysis directory (y/n)?"
-# 	read answer
-# 	if [[ "$answer" != "y" ]]; then
-# 	    exit
-# 	fi
-# 	echo "What's your email (for UPPMAX job error notifications)?"
-# 	read email
-# 	awk -v email=$email '{sub("your@email", email, $0); print $0}' $DIR/cluster.yaml > cluster.yaml
-# 	echo "cluster.yaml copied with email updated. Your can make further changes to the file manually."
-#     fi
-
+if [ "$CLUSTER" = "rackham" ]; then # Change/add cluster name if needed
+    echo "Running on rackham"
     time snakemake \
 	 --conda-frontend mamba \
 	 -j \
 	 --snakefile $DIR/Snakefile \
 	 --use-conda \
-	 --cluster-config cluster.yaml \
+	 --use-singularity \
+	 --cluster-config $DIR/config/cluster.yml \
 	 --cluster " sbatch -J {cluster.name} -A {cluster.account} \
 	             -p {cluster.partition} -n {cluster.n} -t {cluster.time} \
                      {cluster.other} " \
@@ -62,6 +48,8 @@ conda activate $snakemake
     # -j               Tells snakemake all cores available for the job
     # -s               tells which Snakefile to use
     # --use-conda      tells snakemake to create and use rule-specific conda envs
+    # --use-singularty together with a `container: <mycontainer>` in Snakefile,
+    #                  this tells snakemake to run inside the container <mycontainer>
     # --cluster-config tells what cluster-config file to use
     # --cluster " sbatch -J {cluster.name} ..."
     #                  This is the sbatch command with variables
@@ -78,6 +66,7 @@ else
 	 -j \
 	 --snakefile $DIR/Snakefile \
 	 --use-conda \
+	 --use-singularity \
 	 $@
 fi
 
