afa323b191619168019d52bd70c8ad2414500163
bengts <bengt.sennblad@scilifelab.se>
2021-03-26 16:08:52 +0100

    Add two prepend functions to src/common.py

--- src/common.py
+++ src/common.py
@@ -2,7 +2,6 @@
 
 import pandas as pd
 
-
 def read_samples(f):
     df = pd.read_csv(f, index_col=0)
     return df.to_dict(orient="index")
@@ -14,3 +13,19 @@ def dammit_input(wildcards):
     elif wildcards.assembler == "trinity":
         i = "Trinity.fasta"
     return f"results/{wildcards.assembler}/{wildcards.sample}/{i}"
+
+""" It is good keep the code and the results separate, so
+    we run the workflow in a separate working directory. However,
+    we sometimes need to access files in the code folder. This
+    function prepends a file path with the path to the workflow
+    base directory (kept in snakemake variable workflow.basedir) """
+def prependWfd(path):
+    return(os.path.normpath(os.path.join(workflow.basedir, path)))
+
+""" Mostly, snakemake uses relative paths (to the working directory)
+    when referring to, e.g., input and output files. However, when
+    soft-linking (creating aliases) from external file, full paths
+    must be used. This function prepends the path to the current
+    working directory (accessed by the python function os.getcwd()). """
+def prependPwd(path):
+    return(os.path.normpath(os.path.join(os.getcwd(), path)))
